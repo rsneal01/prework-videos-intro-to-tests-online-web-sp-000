ccfa78068275533924123424514a52e81a6c58fc
abstract-metric-6683 <rsneal01@gmail.com>
2020-04-02 17:42:01 +0000

    Done.

--- conversions_test.rb
+++ conversions_test.rb
@@ -8,9 +8,9 @@ else
   puts "Test failed: got #{answer} instead"
 end
 
-puts "Given 1, it should return 28.3495"
-answer = ounces_to_grams(1)
-if answer == 28.3495
+puts "Given 0, it should return 0.0"
+answer = ounces_to_grams(0)
+if answer == 0.0
   puts "Passed!"
 else
   puts "Test failed: got #{answer} instead"
